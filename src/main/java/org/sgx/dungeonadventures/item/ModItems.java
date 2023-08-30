da5b57b8b208755253c6f1682cb8d43a3ad2e60e
Slipum <123569528+Slipum@users.noreply.github.com>
2023-08-30 13:53:42 +0300

    fix brush, add custom dungeon

--- src/main/java/org/sgx/dungeonadventures/item/ModItems.java
+++ src/main/java/org/sgx/dungeonadventures/item/ModItems.java
@@ -2,9 +2,9 @@ package org.sgx.dungeonadventures.item;
 
 import org.sgx.dungeonadventures.DungeonAdventures;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.BrushItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,7 +12,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-	public static final Item Improved_Brush = registerItem("improved_brush", new Item(new FabricItemSettings()));
+	public static final Item Improved_Brush = registerItem("improved_brush", new BrushItem(new Item.Settings()
+			.maxCount(1)
+			.maxDamage(192)));
 
 	private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
 		entries.add(Improved_Brush);
